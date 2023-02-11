cd3bd26ecb8a2b9d6c46a7532e83193c6619bc96
Jayalakshmi Dasari <dasari.jayalakshmi17@gmail.com>
2023-02-12 09:46:38 +1100

    Dropdown option selection using Select

--- src/DropDown.java
+++ src/DropDown.java
@@ -5,13 +5,17 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.By;
 
 public class DropDown {
- public static void main(String[] args) { 
+ public static void main(String[] args) throws InterruptedException { 
 	    String baseURL = "http://demo.guru99.com/test/newtours/register.php";
 	    WebDriver driver = new ChromeDriver();
 	    driver.get(baseURL);
 
 	    Select drpCountry = new Select(driver.findElement(By.name("country")));
 	    drpCountry.selectByVisibleText("AUSTRALIA");
+	    
+	    Thread.sleep(2000);
+
+	    drpCountry.selectByVisibleText("BAHRAIN");
 
 	
 
